46807c73d7c093e448fc91a7b35c1b3d59fb110d
Chris Gamble <gamblecd@cs.washington.edu>
2013-04-28 19:53:05 -0700

    Poor style fix.

--- grails-app/controllers/puppiesplusk9/email/InquiryEmail.groovy
+++ grails-app/controllers/puppiesplusk9/email/InquiryEmail.groovy
@@ -6,6 +6,7 @@ public class InquiryEmail extends AbstractEmail {
     public InquiryEmail(name, email, subject, body) {
         super(name, INTERNAL_ADDRESSES, subject, body);
         this.fromAddress = NO_REPLY_ADDRESS;
+        this.body= this.body + "\n\nREPLY TO: " + this.fromAddress;
     }
     
     protected String buildSubject(name, subject) {
@@ -13,6 +14,6 @@ public class InquiryEmail extends AbstractEmail {
     }
     
     protected String buildBody(body) {
-        return body + "\n\nREPLY TO: " + this.fromAddress;
+        return body;
     }
 }
