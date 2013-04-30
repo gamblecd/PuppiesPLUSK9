ec993f6156cdb53b69070af9c1597d5ef047d814
Chris Gamble <gamblecd@cs.washington.edu>
2013-04-29 19:50:49 -0700

    bettered db connection

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -31,13 +31,17 @@ environments {
                 dbCreate = "update"
                 driverClassName = "org.postgresql.Driver"
                 dialect = org.hibernate.dialect.PostgreSQLDialect
-    
-                uri = new URI(System.env.DATABASE_URL?:"postgres://kfiwruukiqcqum:CpKtm5_vjBjSlM6xmLZkCuH9tS@ec2-54-243-241-130.compute-1.amazonaws.com:5432/d2nebkepkb5u3k")
-
+                dbScheme="postgres"
+                dbHost=System.env.get("db_host");
+                dbPort=System.env.get("db_port");
+                dbUserInfo=System.env.get("db_user")+ ":" + System.env.get("db_pass");
+     
+                
+                uri = new URI(dbScheme, dbUserInfo, dbHost, dbPort);
                 //uri = new URI(System.env.DATABASE_URL?:"postgres://postgres:postgres@127.0.0.1:5432/test")
                 url = "jdbc:postgresql://"+uri.host+uri.path
-                username = uri.userInfo.split(":")[0]
-                password = uri.userInfo.split(":")[1]
+                username = dbUserInfo(":")[0]
+                password = dbUserInfo(":")[1]
         }
     }
 }
