4ebc823737a1d1248219bab5a70b7739571e458c
gamblecd <shadowstorm50@gmail.com>
2013-05-23 16:24:43 -0700

    downgrading to grails 2.2.0

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -20,8 +20,8 @@ environments {
                 driverClassName = "org.postgresql.Driver"
                 dialect = org.hibernate.dialect.PostgreSQLDialect
     
-                uri = new URI(System.env.DATABASE_URL?:"postgres://kfiwruukiqcqum:CpKtm5_vjBjSlM6xmLZkCuH9tS@ec2-54-243-241-130.compute-1.amazonaws.com:5432/d2nebkepkb5u3k")
-                //uri = new URI(System.env.DATABASE_URL?:"postgres://postgres:postgres@127.0.0.1:5432/test")
+                //uri = new URI(System.env.DATABASE_URL?:"postgres://kfiwruukiqcqum:CpKtm5_vjBjSlM6xmLZkCuH9tS@ec2-54-243-241-130.compute-1.amazonaws.com:5432/d2nebkepkb5u3k")
+                uri = new URI(System.env.DATABASE_URL?:"postgres://postgres:postgres@127.0.0.1:5432/test")
                 url = "jdbc:postgresql://"+uri.host+uri.path
                 username = uri.userInfo.split(":")[0]
                 password = uri.userInfo.split(":")[1]        }
